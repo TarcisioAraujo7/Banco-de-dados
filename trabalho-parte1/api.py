6765c2d191523810c4220e76200d589ff59f25b0
Tarcisio Almeida <tarcisio.olv@gmail.com>
2023-04-01 14:02:57 -0300

    pasta trabalho

--- trabalho-parte1/api.py
+++ trabalho-parte1/api.py
@@ -10,19 +10,31 @@ banco_dados = [Pessoa(123456789, "Agatha", datetime.date(2018,5,18))]
 def consultar_usuario(cpf):
     for usuario in banco_dados:
         if usuario.cpf == cpf:
+            print(usuario)
             return jsonify(usuario.retorne_dict())
- #
-@app.route('/usuarios/cadastro', methods=['POST'])  # type: ignore
+    
+    return "Usuario não encontrado"
+
+@app.route('/usuarios/cadastro', methods=['POST']) 
 def cadastrar_usuario():
 
     novo_usuario = request.get_json()
-    print(novo_usuario)
+    
+    for usuario in banco_dados:
+        if usuario.cpf == novo_usuario['cpf']:
+            return 'Ja existe um usuario com este cpf no sistema!'
+
     data_nasc = (novo_usuario['data'])
     data_formatada = datetime.date(data_nasc["ano"],data_nasc["mes"], data_nasc["dia"])
+
     novo_usuario = Pessoa(novo_usuario['cpf'], novo_usuario['nome'], data_formatada)
     
     banco_dados.append(novo_usuario)
 
+    saida = []
+    for usuario in banco_dados:
+        saida.append(usuario.retorne_dict())
 
-    return jsonify(banco_dados)
+    return saida
+    
 app.run(port=5000, host='localhost')
